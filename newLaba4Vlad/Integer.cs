1e55c0344b1d65a9eabc85c21431977d3378d93c
Влад Верожинский <vladyslav.verozhynskyi.itis.2020@lpnu.ua>
2021-05-24 13:17:55 +0300

    new

---
+++ newLaba4Vlad/Integer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newLaba4Vlad
+{
+    public class Integer : Number
+    {
+        public Integer(int num1, int num2)
+        {
+            FirstNum = num1;
+            SecondNum = num2;
+        }
+        public override double Sum()
+        {
+            return FirstNum + SecondNum;
+        }
+
+        public override double Minus()
+        {
+            return FirstNum - SecondNum;
+        }
+
+        public override double Devide()
+        {
+            if (SecondNum != 0) return FirstNum / SecondNum;
+            else return 0;
+        }
+
+        public override double Multiply()
+        {
+            return FirstNum * SecondNum;
+        }
+    }
+}
---
+++ newLaba4Vlad/Number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newLaba4Vlad
+{
+    public abstract class Number
+    {
+        public double FirstNum { get; set; }
+        public double SecondNum { get; set; }
+
+        public abstract double Sum();
+        public abstract double Minus();
+        public abstract double Devide();
+        public abstract double Multiply();
+    }
+}
